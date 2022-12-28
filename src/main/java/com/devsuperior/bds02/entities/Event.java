c676a25d9af8b18a5ec81da4a14158220f6026d2
rodri <rodrigo.porto1403@gmail.com>
2022-12-28 15:52:05 -0300

    Event update

--- src/main/java/com/devsuperior/bds02/entities/Event.java
+++ src/main/java/com/devsuperior/bds02/entities/Event.java
@@ -10,6 +10,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.devsuperior.bds02.dto.EventDTO;
+
 @Entity
 @Table(name = "tb_event")
 public class Event {
@@ -27,7 +29,7 @@ public class Event {
 	
 	public Event() {
 	}
-
+	
 	public Event(Long id, String name, LocalDate date, String url, City city) {
 		this.id = id;
 		this.name = name;
